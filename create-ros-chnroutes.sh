864d2094155159d5505f597242e987f1fe3650d1
Mark Zhang <markz@nvidia.com>
2018-11-05 11:28:06 +0800

    chnroutes: Add script to create ROS chnroutes script
    Signed-off-by: Mark Zhang <markz@nvidia.com>

---
+++ create-ros-chnroutes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget "https://raw.githubusercontent.com/fivesheep/chnroutes/master/chnroutes.py"
+chmod 755 chnroutes.py
+./chnroutes.py -p mac
+echo "/ip firewall address-list" > ros-domestic-ips.rsc
+grep "route add" ip-up >> ros-domestic-ips.rsc
+sed -i -e "s/route add /add address=/g" -e 's/"${OLDGW}"/disabled=no list=domestic/g' ros-domestic-ips.rsc
+sed -i "/add address=10.0.0.0/d" ros-domestic-ips.rsc
+sed -i "/add address=172.16.0.0/d" ros-domestic-ips.rsc
+sed -i "/add address=192.168.0.0/d" ros-domestic-ips.rsc
+rm -f ip-up ip-down chnroutes.py
+echo "Done."
